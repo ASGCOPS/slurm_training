d212992308a4ff771acd021d8ccd8521935a1f67
ASGCOPS <99721014+ASGCOPS@users.noreply.github.com>
2023-07-28 09:46:10 +0800

    Add files

---
+++ 2023/calculate_pi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+##source /etc/profile.d/dicos-environment-modules.sh
+module load app/anaconda3/4.9.2
+python calculate_pi.py
+
---
+++ 2023/mcore_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sbatch mcore.sh -p large
