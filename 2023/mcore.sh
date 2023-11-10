7d1be3a4051b70127b5c0f125d853d7eba62c318
ASGCOPS <99721014+ASGCOPS@users.noreply.github.com>
2023-11-10 16:42:53 +0800

    Update mcore.sh

--- 2023/mcore.sh
+++ 2023/mcore.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=My_MCORE_Job # shows up in the output of 'squeue'
-#SBATCH --time=01:00:00       # specify the requested wall-time
-#SBATCH --nodes=1               # -n number of nodes allocated for this job
-#SBATCH --ntasks-per-node=1     # number of MPI ranks per node
-#SBATCH --cpus-per-task=10      # -c number of OpenMP threads per MPI rank
+#SBATCH --job-name=My_MCORE_Job # define the name of your job
+#SBATCH --time=01:00:00         # specify the required time
+#SBATCH --nodes=1               # number of nodes allocated to the job
+#SBATCH --ntasks-per-node=1     # number of tasks to invoke on each node
+#SBATCH --cpus-per-task=10      # number of CPUs required per task
 #SBATCH --error=job.%J.err      # job error. By default, both files are directed to a file of the name slurm-%j.err
 #SBATCH --output=job.%J.out     # job output. By default, both files are directed to a file of the name slurm-%j.out
 
